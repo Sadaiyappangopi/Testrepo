370bf4f1c25675631ac1de95377094fdfc9dca92
Gobi Sadaiyappan <sadaiyappan.gobi@gmail.com>
2026-04-25 17:44:23 +0530

    bversion2

--- auto_commit.bat
+++ auto_commit.bat
@@ -4,7 +4,7 @@ cd /d "C:\Users\sadai\Documents\AI\01_Claudecode\01_FirstProj"
 git add -A
 
 for /f "tokens=*" %%i in ('git status --porcelain') do (
-    git commit -m "Auto commiit: %DATE% %TIME%"
+    git commit -m "Autooo commiit: %DATE% %TIME%"
     git push origin main
     goto :done
 )
